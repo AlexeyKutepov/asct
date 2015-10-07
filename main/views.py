3cb6bcfe54c1df3473ed40c1dede1ad8b4ab84e8
Alexey Kutepov <alkutepov@gmail.com>
2015-10-07 11:24:19 +0300

    Отображение фото профиля

--- main/views.py
+++ main/views.py
@@ -24,7 +24,14 @@ def index(request):
             }
         )
     elif request.user.user_type == UserProfile.OPERATOR:
-        return render(request, "main/operator_profile.html")
+        probationer_list = UserProfile.objects.filter(user_type=UserProfile.PROBATIONER)
+        return render(
+            request,
+            "main/operator_profile.html",
+            {
+                "probationer_list": probationer_list,
+            }
+        )
     elif request.user.user_type == UserProfile.PROBATIONER:
         return render(request, "main/probationer_profile.html")
     else:
