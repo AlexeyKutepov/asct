adc6b5144ee5cf7da6c05e60a919557054cbba4b
Alexey Kutepov <alkutepov@gmail.com>
2015-11-02 01:29:38 +0300

    Поправил профиль оператора

--- static/js/operator_profile.js
+++ static/js/operator_profile.js
@@ -13,6 +13,42 @@ $(document).ready(function () {
     $("#buttonProbationerSearch").click(probationerSearch);
     $("#inputProbationerSearch").change(probationerSearch).keyup(probationerSearch);
 
+    var journalSearch = function() {
+        $('#tableJournalList > tbody > tr').each(function() {
+            if(($(this).find('a').html()).indexOf($("#inputJournalSearch").val())) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    };
+
+    $("#buttonJournalSearch").click(journalSearch);
+    $("#inputJournalSearch").change(journalSearch).keyup(journalSearch);
+
+    $.ajax({
+        type: "POST",
+        url: "/get/journal/list/",
+        data: {
+            csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
+            id: $( this).attr("property")
+        },
+        success: function(data) {
+            $("#tableJournalList > tbody > tr").each(function() {
+                $(this).remove();
+            });
+            var journalList = data["journal_list"];
+            var result = "";
+            for (var i = 0; i < journalList.length; i++) {
+                result += "<tr><td><a href=\"journal/settings/" + journalList[i]["id"] + "/\" name=\"aJournal\" property=\"" + journalList[i]["id"] + "\">" + journalList[i]["name"] + "</a></td><td>" + journalList[i]["owner"] + "</td></tr>";
+            }
+            $("#tableJournalList").append(result);
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            alert("Error: "+errorThrown+xhr.status+xhr.responseText);
+        }
+    });
+
 });
 
 
