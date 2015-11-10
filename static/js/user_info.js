a72b896cb41a4702eb6c922d9b667be2f2db935c
Alexey Kutepov <alkutepov@gmail.com>
2015-11-10 12:37:46 +0300

    Поправил даты

--- static/js/user_info.js
+++ static/js/user_info.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
                     "<tr>" +
                         "<td><a href=\"/theme/settings/" + userStatisticList[i]["id"] + "/\">" + userStatisticList[i]["name"] +"</a></td>" +
                         "<td>" + userStatisticList[i]["status"] +"</td>" +
-                        "<td>" + userStatisticList[i]["date_from"] +"</td>" +
-                        "<td>" + userStatisticList[i]["date_to"] +"</td>" +
+                        "<td>" + userStatisticList[i]["date_from"].substring(0,10) +"</td>" +
+                        "<td>" + userStatisticList[i]["date_to"].substring(0,10) +"</td>" +
                         "<td>" +
                             "<div class=\"progress\"><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"" + userStatisticList[i]["progress"] + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: " + userStatisticList[i]["progress"] + "%;\"><span class=\"sr-only\">" + userStatisticList[i]["progress"] + "% Complete</span></div></div>" +
                         "</td>" +
