cfedfb8419ca5287e6efd8bbeaf56aa43321cfc4
Alexey Kutepov <alkutepov@gmail.com>
2015-11-19 11:52:51 +0300

    Добавил в интерфейс вкладку для зачётов

--- static/js/theme_settings.js
+++ static/js/theme_settings.js
@@ -59,6 +59,20 @@ $(document).ready(function () {
         }
     });
 
+    $("#aStudy").click(function() {
+        $("#liStudy").addClass( "active" );
+        $("#liExam").removeClass( "active" );
+        $("#divStudy").show();
+        $("#divExam").hide();
+    });
+
+    $("#aExam").click(function() {
+        $("#liStudy").removeClass( "active" );
+        $("#liExam").addClass( "active" );
+        $("#divStudy").hide();
+        $("#divExam").show();
+    });
+
 });
 
 function downloadFile(id) {
