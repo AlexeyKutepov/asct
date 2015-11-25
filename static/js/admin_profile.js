5b4e3e6027705f05781ab6a011efc0d2bd2c6f40
Alexey Kutepov <alkutepov@gmail.com>
2015-11-25 12:04:49 +0300

    Сортировка для администратора

--- static/js/admin_profile.js
+++ static/js/admin_profile.js
@@ -86,5 +86,59 @@ $(document).ready(function () {
         }
     });
 
+    /**
+     * Сортировка таблиц
+     */
+
+    var tableJournalList = $("#tableJournalList");
+
+    $('#thJournalName, #thJournalCompany')
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+        var th = $(this),
+            thIndex = th.index(),
+            inverse = false;
+        th.click(function(){
+            tableJournalList.find('td').filter(function(){
+                return $(this).index() === thIndex;
+            }).sortElements(function(a, b){
+                if( $.text([a]) == $.text([b]) )
+                    return 0;
+                return $.text([a]) > $.text([b]) ?
+                    inverse ? -1 : 1
+                    : inverse ? 1 : -1;
+            }, function(){
+                // parentNode is the element we want to move
+                return this.parentNode;
+            });
+            inverse = !inverse;
+        });
+    });
+
+    var tableUserList = $("#tableUserList");
+
+    $('#thUserName, #thUserPosition, #thUserCompany, #thUserType')
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+        var th = $(this),
+            thIndex = th.index(),
+            inverse = false;
+        th.click(function(){
+            tableUserList.find('td').filter(function(){
+                return $(this).index() === thIndex;
+            }).sortElements(function(a, b){
+                if( $.text([a]) == $.text([b]) )
+                    return 0;
+                return $.text([a]) > $.text([b]) ?
+                    inverse ? -1 : 1
+                    : inverse ? 1 : -1;
+            }, function(){
+                // parentNode is the element we want to move
+                return this.parentNode;
+            });
+            inverse = !inverse;
+        });
+    });
+
 });
 
