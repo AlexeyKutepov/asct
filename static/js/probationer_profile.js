b9c5ab1e930b341464e5e0f5c43d605547354a09
Alexey Kutepov <alkutepov@gmail.com>
2015-12-10 15:35:02 +0300

    Добавил сохранение вкладок на страницу испытуемого

--- static/js/probationer_profile.js
+++ static/js/probationer_profile.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
+    if (supports_html5_storage) {
+        var tab = window.localStorage.getItem("tab");
+        if (tab == "exam") {
+            $("#liStudy").removeClass( "active" );
+            $("#liTest").removeClass( "active" );
+            $("#liExam").addClass( "active" );
+            $("#divTest").hide();
+            $("#divStudy").hide();
+            $("#divExam").show();
+        } else if (tab == "test") {
+            $("#liStudy").removeClass( "active" );
+            $("#liExam").removeClass( "active" );
+            $("#liTest").addClass( "active" );
+            $("#divTest").show();
+            $("#divStudy").hide();
+            $("#divExam").hide();
+        }
+    }
+
     $("#aStudy").click(function() {
         $("#liStudy").addClass( "active" );
         $("#liExam").removeClass( "active" );
@@ -7,6 +26,7 @@ $(document).ready(function () {
         $("#divTest").hide();
         $("#divStudy").show();
         $("#divExam").hide();
+        window.localStorage.setItem("tab", "study")
     });
 
     $("#aExam").click(function() {
@@ -16,6 +36,7 @@ $(document).ready(function () {
         $("#divTest").hide();
         $("#divStudy").hide();
         $("#divExam").show();
+        window.localStorage.setItem("tab", "exam")
     });
 
     $("#aTest").click(function() {
@@ -25,7 +46,20 @@ $(document).ready(function () {
         $("#divTest").show();
         $("#divStudy").hide();
         $("#divExam").hide();
+        window.localStorage.setItem("tab", "test")
     });
 
 });
+
+/**
+ * Проверяем доступность локального хранилища
+ * @returns {boolean}
+ */
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+} catch (e) {
+    return false;
+  }
+}
 
