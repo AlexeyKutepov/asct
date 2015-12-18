6fda786b565106aaa6a9a2b7b8603468ea3834d8
Alexey Kutepov <alkutepov@gmail.com>
2015-12-18 15:22:15 +0300

    Реализованы пункты 1 - 3 из письма Георгия от 17 декабря 2015 года

--- static/js/curator_profile.js
+++ static/js/curator_profile.js
@@ -231,5 +231,55 @@ $(document).ready(function () {
         });
     });
 
+    var tableCompanyList = $("#tableCompanyList");
+
+    $('#thCompanyName')
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+        var th = $(this),
+            thIndex = th.index(),
+            inverse = false;
+        th.click(function(){
+            tableCompanyList.find('td').filter(function(){
+                return $(this).index() === thIndex;
+            }).sortElements(function(a, b){
+                if( $.text([a]) == $.text([b]) )
+                    return 0;
+                return $.text([a]) > $.text([b]) ?
+                    inverse ? -1 : 1
+                    : inverse ? 1 : -1;
+            }, function(){
+                // parentNode is the element we want to move
+                return this.parentNode;
+            });
+            inverse = !inverse;
+        });
+    });
+
+    var tablePositionList = $("#tablePositionList");
+
+    $('#thPositionName')
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+        var th = $(this),
+            thIndex = th.index(),
+            inverse = false;
+        th.click(function(){
+            tablePositionList.find('td').filter(function(){
+                return $(this).index() === thIndex;
+            }).sortElements(function(a, b){
+                if( $.text([a]) == $.text([b]) )
+                    return 0;
+                return $.text([a]) > $.text([b]) ?
+                    inverse ? -1 : 1
+                    : inverse ? 1 : -1;
+            }, function(){
+                // parentNode is the element we want to move
+                return this.parentNode;
+            });
+            inverse = !inverse;
+        });
+    });
+
 });
 
