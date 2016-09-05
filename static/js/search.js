185507674acd7c93d67ab39e9d0fce7e05b11047
Alexey Kutepov <alkutepov@gmail.com>
2016-09-05 13:24:04 +0300

    Добавил поиск и сортировку уволенных сотрудников

--- static/js/search.js
+++ static/js/search.js
@@ -18,5 +18,22 @@ $(document).ready(function () {
 
     $("#buttonUserSearch").click(userSearch);
     $("#inputUserSearch").change(userSearch).keyup(userSearch);
+
+    /**
+     * Поиск по таблице уволенных сотрудников
+     */
+    var fireUserSearch = function() {
+        $('#tableFireUserList > tbody > tr').each(function() {
+            if(($(this).find('a').html().toUpperCase()).indexOf($("#inputFireUserSearch").val().toUpperCase()) > -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    };
+
+    $("#buttonFireUserSearch").click(fireUserSearch);
+    $("#inputFireUserSearch").change(fireUserSearch).keyup(fireUserSearch);
+
 });
 
